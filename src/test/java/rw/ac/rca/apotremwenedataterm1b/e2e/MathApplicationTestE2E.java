82439db3611268518616c5394492a1a328c939b2
Hallcoder <hallcoder25@gmail.com>
2023-12-18 11:41:54 +0200

    [feat]: end to end tests

---
+++ src/test/java/rw/ac/rca/apotremwenedataterm1b/e2e/MathApplicationTestE2E.java
@@ -0,0 +1,38 @@
+package rw.ac.rca.apotremwenedataterm1b.e2e;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MathApplicationTestE2E {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testApiEndpoint() {
+        // Given
+        String url = "http://localhost:" + port + "/api/v1/do_math";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String requestBody = "{\"operand1\": 2, \"operand2\": 5, \"operation\": \"+\"}";
+
+        // When
+        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
+        String response = restTemplate.postForObject(url, request, String.class);
+
+        // Then
+        assertEquals("Expected Response", response);
+    }
+}
